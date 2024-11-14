d76a5b88ed7e8899848b6fa625391489aa3790bb
JessenFB <jessenfallikbemi_2327250065@mhs.mdp.ac.id>
2024-11-14 20:16:30 +0700

    p

--- flask-template/main.py
+++ flask-template/main.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_tempalet,request,redirect,url_for
-app=Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for
 
-@app.route('/contact',method={'GET','POST'})
+app = Flask(__name__)
+
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        name=request.form['name']
-        email=request.form['email']
-        message=request.form['message']
-        print(f"Name :{name},Email :{email},Message {message}")
-        return redirect(url_for('contact'))
-    
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        
+        print(f"Name: {name}, Email: {email}, Message: {message}")
+        
+        # You could add form validation or save to a database here
+
+        return redirect(url_for('contact'))  # Redirect back to the contact page after form submission
+
     title = "Contact Page"
-    return render_tempalet('contacts.html',title-title)
+    return render_template('contact.html', title=title)
 
-
-
-
-if __name__ =='__main__':
-    app.run(debug=True)   
+if __name__ == '__main__':
+    app.run(debug=True)
